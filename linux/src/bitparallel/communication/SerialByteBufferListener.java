6b4bc0e8b4f87cac1b8db211eda1cb52d872d7b5
Max van Daalen <max.vandaalen@gmail.com>
2021-08-18 14:20:39 +0100

    Renamed the listener interface method

--- linux/src/bitparallel/communication/SerialByteBufferListener.java
+++ linux/src/bitparallel/communication/SerialByteBufferListener.java
@@ -1,12 +1,12 @@
 package bitparallel.communication;
 
 //
-// (c) Bit Parallel Ltd, December 2020
+// (c) Bit Parallel Ltd, August 2021
 //
 
 import java.nio.ByteBuffer;
 
 public interface SerialByteBufferListener
 {
-    public void serialRxedByteBuffer(final ByteBuffer buffer);
+    public void rxedSerialByteBuffer(final ByteBuffer buffer);
 }
